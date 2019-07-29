e3c09b5396f181e2707c899e2e9f6920738be23d
David Wolak <davidw4@co.multnomah.or.us>
2019-07-29 12:53:57 -0700

    Changed googleAPI key and removed appsettings which now contains the key

---
+++ BasicMVCWebsite/Models/MyConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicMVCWebsite.Models
+{
+    public class MyConfig
+    {
+        public string GOOGLEKEY { get; set; }
+    }
+}
